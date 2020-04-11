346c223653343706847cbe87f20ccf0a63a7d51d
Sumanshu Nankana <sumanshunankana@gmail.com>
2020-04-11 10:12:28 +0100

    pylint tutorials added

---
+++ Pylint/code02.py
@@ -0,0 +1,18 @@
+#
+# Pylint Tutorial
+#
+
+"""Pylint Practice"""
+
+class Car(object):
+    """Example Docstring"""
+    def __init__(self, color):
+        self.color = color
+
+MY_CAR = Car('blue')
+
+def crash(car1, car2):
+    """Example Fun Docstring"""
+    car1.color = 'burnt'
+
+crash(Car('red'), MY_CAR)
---
+++ Pylint/code04.py
@@ -0,0 +1,19 @@
+#
+# Pylint Tutorial
+#
+
+"""Pylint Practice"""
+
+#pylint: disable=too-few-public-methods
+class Car(object):
+    """Example Docstring"""
+    def __init__(self, color):
+        self.color = color
+
+MY_CAR = Car('blue')
+
+def crash(car1, car2):  #pylint: disable=unused-argument
+    """Example Fun Docstring"""
+    car1.color = 'burnt'
+
+crash(Car('red'), MY_CAR)
